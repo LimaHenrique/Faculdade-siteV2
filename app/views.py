0ab2fcd9d929945ccab30c7890ceca1d7969dd9a
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-14 17:22:43 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,36 +1,12 @@
 from django.shortcuts import render
 
 def index(request):
-    context = {
-        'title' : 'Titulo do site',
-        'header' : 'header do site',
-        'footer' : 'Rodape do site' ,
-        'conteudo' : 'Aqui ira seu conteudo do site',
-        'lista':[
-              'Template 1' ,
-              'Template 2',
-              'Template 3',
-              'Template 4',
-              'Template 5',
-              'Template 6',
-              'Template 7',
-              'Template 8',
-              'Template 9',
-        ] 
-    }
     return render(request, "index.html")
-
 def contato(request):
-    return render(request, "contato.html" )
-
-def blog(request):
-    return render(request, "blog.html" )
-
-def eventos(request):
-    return render(request, "eventos.html" )
-
+    return render(request, "contato.html")
 def cursos(request):
-    return render(request, "cursos.html" )    
-    
+    return render(request, "cursos.html")
+def eventos(request):
+    return render(request, "eventos.html")
 def inscricao(request):
-    return render(request, "inscricao.html" )        
+    return render(request, "inscricao.html")
