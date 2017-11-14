808993269e98257dd517bfba0ac0c4b3ab53e8fc
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-14 16:48:52 -0200

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import Contato
 
 def index(request):
     context = {
@@ -23,26 +21,16 @@ def index(request):
     return render(request, "index.html")
 
 def contato(request):
-    form = Contato()
-    context = { "contato.html" : form }
     return render(request, "contato.html" , context)
 
 def blog(request):
-    form = Contato()
-    context = { "blog.html" : form }
     return render(request, "blog.html" , context)
 
 def eventos(request):
-    form = Contato()
-    context = { "eventos.html" : form }
     return render(request, "eventos.html" , context)
 
 def cursos(request):
-    form = Contato()
-    context = { "cursos.html" : form }
     return render(request, "cursos.html" , context)    
     
 def inscricao(request):
-    form = Contato()
-    context = {"inscricao.html" : form }
     return render(request, "inscricao.html" , context)        
