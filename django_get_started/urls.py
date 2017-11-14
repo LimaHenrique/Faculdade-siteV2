b2407178a78e9c9a0c8b913bec6c5a7dba171515
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-14 17:12:25 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,8 +1,7 @@
+
 from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
-from app.views import *
-from django.views.generic import RedirectView
+from core.views import *
 
 
 urlpatterns = [
