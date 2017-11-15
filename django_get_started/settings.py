1d1a3b5de6b90181eb664d01cb6b745c7a56c13d
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 01:30:28 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -1,17 +1,25 @@
- -"""
- -Django settings for django_get_started project.
- -"""
- +
-  from os import path
-  import os
+"""
+Django settings for django_get_started project.
+"""
+
+from os import path
+import os
 mysqlconnstr = os.environ['MYSQLCONNSTR_localdb']
 mysqlconnlst = mysqlconnstr.split(';')
 mysqlconndict = dict(s.split('=',1) for s in mysqlconnlst)
 
-
 PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
 DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ALLOWED_HOSTS = {'*'}
+
+ADMINS = (
+    # ('Your Name', 'your_email@example.com'),
+)
+
+MANAGERS = ADMINS
 
 with open('\home\data\mysql\MYSQLCONNSTR_localdb.txt') as arquivo:
     linha = arquivo.read()
@@ -28,111 +36,121 @@ DATABASES = {
         'PORT': x['Data Source'].split(':')[1],
     }
 }
- -# Local time zone for this installation. Choices can be found here:
- -# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
- -# although not all choices may be available on all operating systems.
- -# On Unix systems, a value of None will cause Django to use the same
- -# timezone as the operating system.
- -# If running in a Windows environment this must be set to the same as your
- -# system time zone.
-  TIME_ZONE = 'America/Sao_Paulo'
-  
- -# Language code for this installation. All choices can be found here:
- -# http://www.i18nguy.com/unicode/language-identifiers.html
-  LANGUAGE_CODE = 'pt-br'
-  
- -# If you set this to False, Django will make some optimizations so as not
- -# to load the internationalization machinery.
-  USE_I18N = True
- -
- -# If you set this to False, Django will not format dates, numbers and
- -# calendars according to the current locale.
-  USE_L10N = True
-  
- -# If you set this to False, Django will not use timezone-aware datetimes.
-  USE_TZ = True
-  
-  STATIC_URL = '/static/'
- -
- -# Make this unique, and don't share it with anybody.
-  SECRET_KEY = 'n(bd1f1c%e8=_xad02x5qtfn%wgwpi492e$8_erx+d)!tpeoim'
-  
- -# List of callables that know how to import templates from various sources.
-  TEMPLATE_LOADERS = (
-      'django.template.loaders.filesystem.Loader',
-      'django.template.loaders.app_directories.Loader',
-  ROOT_URLCONF = 'django_get_started.urls'
-  
- -# Python dotted path to the WSGI application used by Django's runserver.
-  WSGI_APPLICATION = 'django_get_started.wsgi.application'
-  
- -TEMPLATE_DIRS = (
- -    # Put strings here, like "/home/html/django_templates" or
- -    # "C:/www/django/templates".
- -    # Always use forward slashes, even on Windows.
- -    # Don't forget to use absolute paths, not relative paths.
- -)
- -
-   INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'app',
-]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+LOGIN_URL = '/login'
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = ''
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = ''
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = '/static/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'n(bd1f1c%e8=_xad02x5qtfn%wgwpi492e$8_erx+d)!tpeoim'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-  INSTALLED_APPS = (
-      'django.contrib.admin',
-      'django.contrib.auth',
-          'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-      },
-  ]
-   TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
-   AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
- -# A sample logging configuration. The only tangible logging
- -# performed by this configuration is to send an email to
- -# the site admins on every HTTP 500 error when DEBUG=False.
- -# See http://docs.djangoproject.com/en/dev/topics/logging for
- -# more details on how to customize your logging configuration.
-  LOGGING = {
+ROOT_URLCONF = 'django_get_started.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'django_get_started.wsgi.application'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or
+    # "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app',
+    # Uncomment the next line to enable the admin:
+    # 'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+)
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'filters': {
@@ -150,10 +168,11 @@ MIDDLEWARE = [
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'level': 'ERROR',A
             'propagate': True,
         },
     }
 }
- -# Specify the default test runner.
-  TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+# Specify the default test runner.
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
