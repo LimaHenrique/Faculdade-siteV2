58749240feb080e47b2c284b2158330e335e43c3
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 01:53:34 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -125,12 +125,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-
 
 # Specify the default test runner.
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
