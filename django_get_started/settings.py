dca418e52faa358437907c283249be21fe951c64
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 03:57:13 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -55,7 +55,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'n(bd1f1c%e8=_xad02x5qtfn%wgwpi492e$8_erx+d)!tpeoim'
