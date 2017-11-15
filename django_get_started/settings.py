cc55d110bc1a79751659344faa15895a50dd6588
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 02:42:18 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -57,7 +57,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../app/static'),
 )
 
 # Make this unique, and don't share it with anybody.
