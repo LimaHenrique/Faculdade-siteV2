5b7775fa77c5ef633feccefff60ce7ee18fa774f
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 02:27:17 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -2,9 +2,6 @@
 from django.conf.urls import url
 from django.contrib import admin
 from app.views import * 
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -17,4 +14,3 @@ urlpatterns = [
     url(r'^blog.html', blog)
     
 ]
-static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
