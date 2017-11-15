91781713917b38a7a56e3c7290b3b93ed701ebc4
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 02:19:41 -0200

    Update urls.py

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,7 +1,8 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from app.views import *from django.conf import settings
+from app.views import * 
+from django.conf import settings
 from django.conf.urls.static import static
 
 
