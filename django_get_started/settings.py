c0d06ea3582e639267081f90469150cbdf2fb11a
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 01:02:38 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -78,7 +78,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+STATIC_ROOT= os.path.join(PROJECT_DIR,'static_media/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -177,4 +177,3 @@ LOGGING = {
 # Specify the default test runner.
 TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
-STATIC_URL = '/static/'
