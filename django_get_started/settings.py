f5df0a676d480b40a68bebbee7f4b7292f55a172
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-15 02:05:03 -0200

    Update settings.py

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -56,6 +56,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'n(bd1f1c%e8=_xad02x5qtfn%wgwpi492e$8_erx+d)!tpeoim'
 
