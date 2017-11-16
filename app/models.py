cef658d10ba9e509ea19661b4aee27cd852f6b73
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-16 16:37:01 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,35 +2,6 @@ from django.test import TestCase
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
-class Usuario(AbstractBaseUser):
-      nome = models.CharField(max_length=50)
-      ra = models.IntegerField(unique=True)
-      password = models.CharField(max_length=100)
-      perfil = models.CharField(max_length=1, default='C')
-      ativo = models.BooleanField(default=True)
-
-      USERNAME_FIELD = 'ra'
-      REQUIRED_FIELDS = ['nome']
-
-      objects = UsuarioManager()
-
-      @property
-      def is_staff(self):
-            return self.perfil == 'C'
-
-      def has_perm(self, perm, obj=None):
-            return True
-      
-      def has_module_perms(self, app_label):
-            return True
-  
-      def get_short_name(self):
-            return self.nome
-      def get_full_name(self):
-            return self.nome
-      
-      def __str__(self):
-            return self.nome
 
 class UsuarioManager(BaseUserManager):
       use_in_migrations = True
