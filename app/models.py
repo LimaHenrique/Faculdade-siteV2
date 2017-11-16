ec9b1ad7e0eeb468c27697bf0703e8dfb80863eb
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-16 16:34:38 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
 
+class Curso(models.Model):
+      sigla = models.CharField(max_length=5)
+      nome = models.CharField(max_length=200)
+
+      def __str__(self):
+            return self.nome
+
 class UsuarioManager(BaseUserManager):
       use_in_migrations = True
       def _create_user(self, ra, password, **extra_fields):
