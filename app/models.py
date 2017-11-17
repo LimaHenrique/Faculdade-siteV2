4609d65a1e015ae8efcdce07da19286e3fd4ba27
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-17 14:34:19 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.db import models
 
-# Create your models here.
+''' Create your models here.
 class app_aluno(models.Model):
    nome = models.CharField(max_length=200)
    ra = models.IntegerField(max_length=7)
