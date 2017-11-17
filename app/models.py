0baf90374eb1389a4214a49c08cf5546e6ad7c92
LimaHenrique <henrique_margarido@hotmail.com>
2017-11-17 11:14:28 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.db import models
- 
- # Create your models here.
- class Curso(models.Model):
-     nome = models.CharField(max_length=200)
-     periodo = models.CharField(max_length=50)
-     instituicao = models.CharField(max_length=200)
- 
- class Vestibular(models.Model):
-     nome = models.CharField(max_length=200)
+
+# Create your models here.
+class Curso(models.Model):
+   nome = models.CharField(max_length=200)
+   periodo = models.CharField(max_length=50)
+   instituicao = models.CharField(max_length=200)
+    
+class Vestibular(models.Model):
+   nome = models.CharField(max_length=200)
